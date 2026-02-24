2cb06b20eaf95d6816f1f5a0f804c82fbc11c124
Beeemba <streletsvlad@yandex.ru>
2026-02-24 10:30:19 +0500

    2lab3

---
+++ lab02/3 question.py
@@ -0,0 +1,24 @@
+def dl(n):
+    d=[]
+    for i in range(2,int(n**0.5)+1):
+        if n % i == 0 :
+            d.append(i)
+            if i != n //i:
+                d.append(n//i)
+    return(d)
+ds=[]
+def ps(n):
+    for i in dl(n):
+        if i % 10 == 0 and i != 8 and i != n:
+            return i
+    return None
+    
+
+res=[]
+nc=500_001
+while len(res) < 5:
+    d=ps(nc)
+    if d:
+        res.append((nc,d))
+        print(f"{nc} {d}")
+    nc+=1
