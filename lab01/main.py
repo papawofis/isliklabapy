de215010e294b86aa75f0713678b7b6770188d8d
papawofis <isliktapok@gmail.com>
2026-03-29 20:53:08 +0500

    Update main.py

--- lab01/main.py
+++ lab01/main.py
@@ -9,3 +9,22 @@ sites = {
 
 distances = get_distances(sites)
 print(distances)
+
+#01.cir
+from circle import area, in_circle
+
+radius = 42
+
+# Площадь круга с точностью 4 знака
+s = area(radius)
+print(f"{s:.4f}")
+
+# Первая точка
+point = (23, 34)
+print(in_circle(point, radius))
+
+# Вторая точка
+point2 = (30, 30)
+print(in_circle(point2, radius))
+
+#02.oper
