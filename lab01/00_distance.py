f2c81e20959e07e0449ac24178f05a67629710e5
papawofis <isliktapok@gmail.com>
2026-03-29 20:49:10 +0500

    Update 00_distance.py

--- lab01/00_distance.py
+++ lab01/00_distance.py
@@ -1,27 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Есть словарь координат городов
+def dist(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
-sites = {
-    'Moscow': (550, 370),
-    'London': (510, 510),
-    'Paris': (480, 480),
-}
-
-
-# расстояние на координатной сетке - ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
-
-distances = {
-    'Moscow ': {'London':((550-510)**2 + (370-578)**2)**0.5,'Paris':((550-480)**2 + (370-480)**2)**0.5},
-    'London': {'Moscow':((550-510)**2 + (370-578)**2)**0.5,'Paris':((510-480)**2 + (510-480)**2)**0.5},
-    'Paris': {'London':((510-480)**2 + (510-480)**2)**0.5,'Moscow':((550-480)**2 + (370-480)**2)**0.5},
-    }
-
-# TODO здесь заполнение словаря
-
-print(distances)
-
-
-
-
+def get_distances(cities):
+    result = {}
+    names = list(cities.keys())
+    
+    for i, name1 in enumerate(names):
+        result[name1] = {}
+        for j, name2 in enumerate(names):
+            if i != j:
+                result[name1][name2] = dist(cities[name1], cities[name2])
+    
+    return result
